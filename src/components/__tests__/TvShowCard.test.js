16bddee9bdacf94d4878c56a0906a0ff30b9848b
markinFront <marcosviniciuslimavieira8@gmail.com>
2024-02-05 15:22:55 -0300

    Implementando testes com Vitest --TvShowCard

---
+++ src/components/__tests__/TvShowCard.test.js
@@ -0,0 +1,40 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import TvShowCard from "../TvShowCard.vue";
+
+describe("TvShowCard Component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    // Montar o componente antes de cada teste
+    wrapper = mount(TvShowCard, {
+      props: {
+        tvShow: {
+          title: "Breaking Bad",
+          genre_ids: [1, 2],
+        },
+        genres: [
+          { id: 1, name: "Drama" },
+          { id: 2, name: "Comedy" },
+        ],
+      },
+      global: {
+        // Inclua mocks ou plugins globais necessários
+      },
+    });
+  });
+
+  afterEach(() => {
+    // Desmontar o componente após cada teste
+    if (wrapper) {
+      wrapper.unmount();
+    }
+  });
+
+  it("Renderiza corretamente", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  
+
+});
